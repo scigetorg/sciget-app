0bd764074dbc4044ed8b6e74096a59ec73cbf045
iishiishii <love_sweet_98@yahoo.com.vn>
2023-04-26 15:50:24 +1000

    add entry for jlab_version to check for updates. set neurodesk version when installing

--- scripts/get_package_version.py
+++ scripts/get_package_version.py
@@ -5,7 +5,7 @@ def get_package_version(path):
     """Extract version from given `package.json` file."""
     with open(path) as f:
         package = json.load(f)
-        return package['version']
+        return package['jlab_version']
 
 if __name__ == '__main__':   
     print(get_package_version(path='package.json'))
