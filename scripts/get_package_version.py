409059c2b1839a4ee73f10b7af82544d5a3133ab
iishiishii <love_sweet_98@yahoo.com.vn>
2025-01-31 11:37:24 +1000

    get tinyrange release

--- scripts/get_package_version.py
+++ scripts/get_package_version.py
@@ -1,11 +1,24 @@
 import json
+import argparse
 
-
-def get_package_version(path):
+def get_package_version(path, package_name):
     """Extract version from given `package.json` file."""
     with open(path) as f:
         package = json.load(f)
-        return package['version']
+        if package_name == 'neurodesk':
+            return package['version']
+        elif package_name == 'tinyrange':
+            return package['tinyrange_version']
+        else:
+            raise ValueError(f"Unknown package: {package}")
 
 if __name__ == '__main__':   
-    print(get_package_version(path='package.json'))
+    parser = argparse.ArgumentParser(
+        prog="Get package version from package.json",
+    )
+    
+    parser.add_argument("--package_name", type=str, required=True, help="Package name")
+
+    args = parser.parse_args()
+
+    print(get_package_version(path='package.json', package_name=args.package_name))
